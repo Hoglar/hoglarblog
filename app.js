45d8c784f781224148f525bdf0abcc9723851a35
Martin <martinhl__@hotmail.com>
2017-01-10 19:42:26 +0100

    added some text to app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('<h1>Hoglar Blog</h1>');
+    res.send('<h1>Hoglar Blog, running on side</h1>');
 });
 
 app.listen(3000, () => {
