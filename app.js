c665f3dc8a88a83780659e0dd115a1cee00c54e1
Martin <martinhl__@hotmail.com>
2017-01-11 20:45:04 +0100

    Commit før push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('<h1>Hoglar Blog, running on side</h1>');
+    res.send('<h1>Hoglar Blog, running on side</h1>\n <h2>Hei på deg</h2>');
 });
 
 app.listen(3000, () => {
