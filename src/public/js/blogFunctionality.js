79358d10d2e617ea88be2abbadeda3f83097cd9b
Martin <martinhl__@hotmail.com>
2017-02-21 11:39:04 +0100

    Gjort en del i dag,byttet bilde, og jobbet med glosene

--- src/public/js/blogFunctionality.js
+++ src/public/js/blogFunctionality.js
@@ -1,7 +1,17 @@
 window.onload = () => { 
-    document.getElementById("glossaryh1Selector").onclick = () => {
-        const glossaryContent = document.getElementById("glossaryContent");
-        glossaryContent.style.display = "initial";
-    }
+    const glossaryContent = document.getElementById("glossaryContent");
+    glossaryContent.style.display = "none"
+    
+    document.getElementById("glossaryh1Selector").addEventListener("click", function() {
+        
+        
+        if (glossaryContent.style.display === "none") {
+            glossaryContent.style.display = "initial";
+            
+        } else if (glossaryContent.style.display === "initial"){
+            glossaryContent.style.display = "none"
+        }
+         
+    });
 
 };
