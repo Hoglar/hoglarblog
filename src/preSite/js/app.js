96d8b22cdb7949b9c1e59c9a351ee3a757c12f9e
Hoglar <martinhl__@hotmail.com>
2018-03-07 13:30:22 +0100

    sdsad

--- src/preSite/js/app.js
+++ src/preSite/js/app.js
@@ -5,4 +5,15 @@ import ReactDOM from 'react-dom';
 
 import '../css/styles.css';
 
-ReactDom.render(<Application /> , document.getElementById('root'));
+class Application extends React.Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <h1>Hello world!</h1>
+            </div>
+
+        )
+    }
+}
+
+ReactDOM.render(<Application /> , document.getElementById('root'));
