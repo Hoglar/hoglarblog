a3c114623e927c2f0ed6e3c84d27332cc86a0966
Hoglar <martinhl__@hotmail.com>
2018-03-11 21:42:00 +0100

    sdsd

--- src/preSite/js/app.js
+++ src/preSite/js/app.js
@@ -6,28 +6,50 @@ import PropTypes from 'prop-types';
 
 import '../css/styles.css';
 
-import home from '../assets/';
+import home from '../assets/house.png';
 
 
-
+// må også gjøre dette mer modulært.
 
 
 // Sidebar indeholder sidebar boxes, det er disse boxene som er
 // hovedgreia, kanskje det ikke trenger være et helt component.
 
+
+// Jeg lager et object som bestemmer hvordan sidebarBoxen skal se ut,
+// Objectet trenger hvilken elementer jeg vil ha i sidebaren
+// objectet må også kunne si hvilken rekkefølge.
+
+// elementene en kan velge mellom vil etterhvert være egne komponeneter,
+// i sidebaren trenger jeg kun deres gjeldene bilder.
+
+const defaultSidebar = [
+    {
+        img: home,
+        id: 1,
+    },
+    {
+        img: home,
+        id: 2,
+    }
+]
+
+// Sidebar får hittil en props. Den får et object fra Application.
+
 class Sidebar extends React.Component {
     render() {
         return (
             <div className="sidebar">
-                <SidebarBox img={home}/>
-                <SidebarBox />
-                <SidebarBox />
-                <SidebarBox />
-                <SidebarBox />
-                <SidebarBox />
+                {this.props.sidebarLayout.map(function(api, index) {
+                    return (
+                        <SidebarBox
+                            img={api.img}
+                            key={api.id} />
+                    )
+                }.bind(this))}.
+
                 <div className="sidebarFiller"/>
             </div>
-
         )
     }
 }
@@ -39,7 +61,7 @@ class SidebarBox extends React.Component {
     render() {
         return(
             <div className="sidebarBox">
-                <img src={this.props.img} />
+                <img src={this.props.img} className="center" />
             </div>
         )
     }
@@ -53,7 +75,7 @@ class Application extends React.Component {
     render() {
         return (
             <div className="wrapper">
-                <Sidebar />
+                <Sidebar sidebarLayout={defaultSidebar}/>
 
             </div>
         )
