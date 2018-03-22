084aac88e4e05b215c6abd286b1360fc5a058b2c
Hoglar <martinhl__@hotmail.com>
2018-03-22 23:25:37 +0100

    Satt opp fetch call to api

--- src/preSite/js/app.js
+++ src/preSite/js/app.js
@@ -143,6 +143,18 @@ class Application extends React.Component {
             showDictionary: false,
         }
     }
+
+    componentDidMount() {
+        console.log("did mount");
+        axios.get('/api')
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            })
+    }
+
     // I want the footer to show only on scrolling,
     // rest of the page is on page.
     showFooter(e) {
