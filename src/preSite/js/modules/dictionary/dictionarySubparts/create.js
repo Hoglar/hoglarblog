ab404d7e7f8bc28af46701e7e0778eaf300b2fce
Martin <martin.hoglar@gmail.com>
2018-05-16 21:01:18 +0200

    Starter på styling av create!

--- src/preSite/js/modules/dictionary/dictionarySubparts/create.js
+++ src/preSite/js/modules/dictionary/dictionarySubparts/create.js
@@ -7,12 +7,22 @@ export default class DictionaryCreate extends React.Component {
         return (
             <div className="dictionaryCreate">
                 <form>
-
                     <label for="dictionaryCreateTopic">Topic</label>
                     <input id="dictionaryCreateTopic" type="text" value={(this.props.topic) ? this.props.topic : ""}></input>
 
                     <label for="dictionaryCreateTitle">Title</label>
                     <input id="dictionaryCreateTitle" type="text"></input>
+
+                    <label for="dictionaryCreateExplanation">Explanation</label>
+                    <input id="dictionaryCreateExplanation" type="textarea"></input>
+
+                    <label for="dictionaryCreateExample">Example</label>
+                    <input id="dictionaryCreateExample" type="textarea"></input>
+
+                    <label for="dictionaryCreateReference">Reference</label>
+                    <input id="dictionaryCreateReference" type="textarea"></input>
+
+                    <input type="submit">Save to dictionary</input>
 
                 </form>
             </div>
