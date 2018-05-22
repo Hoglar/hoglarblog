9031d942d45c0206384f505589c63c97cd05841d
Martin <martin.hoglar@gmail.com>
2018-05-22 22:44:59 +0200

    Its good now, we need to get rid of search and focus title.

--- src/preSite/js/modules/dictionary/dictionarySubparts/create.js
+++ src/preSite/js/modules/dictionary/dictionarySubparts/create.js
@@ -8,7 +8,6 @@ export default class DictionaryCreate extends React.Component {
         event.preventDefault();
 
         //handleCreateSubmit(topic, title, explanation, example, reference)
-
         let createData = {
             topic: this.refs.topic.value,
             title: this.refs.title.value,
@@ -32,8 +31,7 @@ export default class DictionaryCreate extends React.Component {
                     <div className="dictionaryCreateFormTop">
                         <input ref="topic" type="text" value={this.props.topic} hidden></input>
 
-                        <label for="dictionaryCreateTitle">Title</label>
-                        <input id="dictionaryCreateTitle" ref="title" type="text"></input>
+                        <input className="dictionaryCreateTitle" ref="title" type="text" placeholder="Title:"></input>
                     </div>
                     <div className="dictionaryCreateInputSection">
                         <textarea id="dictionaryCreateExplanation" ref="explanation" type="text" placeholder="Explanation:"/>
@@ -41,7 +39,7 @@ export default class DictionaryCreate extends React.Component {
                         <textarea id="dictionaryCreateExample" ref="example" type="text" placeholder="Example:"/>
 
                         <textarea id="dictionaryCreateReference" ref="reference" type="text" placeholder="Reference:"/>
-                        <button type="submit" onClick={this.handleSubmit.bind(this)}>Save</button>
+                        <button className="dictionaryCreateInputButton" type="submit" onClick={this.handleSubmit.bind(this)}>Save</button>
                     </div>
                 </form>
             </div>
