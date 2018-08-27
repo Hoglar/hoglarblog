3d94c890a24184c91b3b4417456b1fbde62a7a29
Martin <martin.hoglar@gmail.com>
2018-08-27 12:29:36 +0200

    Fixed some layout,

--- src/preSite/js/modules/sidebar/sidebarBox.js
+++ src/preSite/js/modules/sidebar/sidebarBox.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-// Gets onclick and sshowBoxInfo
+// Gets onclick and showBoxInfo
 export default class SidebarBox extends React.Component {
     constructor(props) {
         super(props)
