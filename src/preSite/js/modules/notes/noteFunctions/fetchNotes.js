6ce970949f2c0568210c1fab482d005ea4a66e14
Hoglar <martinhl__@hotmail.com>
2018-09-23 20:34:17 +0200

    Må sette opp endpoint.

--- src/preSite/js/modules/notes/noteFunctions/fetchNotes.js
+++ src/preSite/js/modules/notes/noteFunctions/fetchNotes.js
@@ -1,9 +1,20 @@
 'use strict';
 
+
 export default function fetchNotes(searchData, topic) {
+
+    const url = "/api/notes/notesSearch?searchData=";
+
 
     return new Promise((resolve, reject) => {
         console.log("We got here!");
 
+        fetch(url + searchData + "&topic=" + topic)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            console.log(response);
+        })
     })
 }
