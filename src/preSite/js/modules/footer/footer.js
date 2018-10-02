42fc4b85b95cd65404188129f80af0099d594d3e
Hoglar <martinhl__@hotmail.com>
2018-10-02 20:57:21 +0200

    Ok, sjekk notes nå.

--- src/preSite/js/modules/footer/footer.js
+++ src/preSite/js/modules/footer/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Footer extends React.Component {
     render() {
         return (
-            <div className="footerWrapper">
+            <div className="footer">
                 <div className="supportSection">
                     Support project with Bitcoin: 3AtBGhxf1nAYp5kHBTdivQuVfVE99cZYtj
                 </div>
