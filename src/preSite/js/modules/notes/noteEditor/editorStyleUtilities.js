60ff60e10b378438ccdd779fa5a3be060bb09d1f
Martin <martin.hoglar@gmail.com>
2019-02-28 10:09:33 +0100

    OK, fikse litt på blockQuoten

--- src/preSite/js/modules/notes/noteEditor/editorStyleUtilities.js
+++ src/preSite/js/modules/notes/noteEditor/editorStyleUtilities.js
@@ -32,5 +32,4 @@ const INLINE_STYLES = [
     {label: 'Bold', style: 'BOLD'},
     {label: 'Italic', style: 'ITALIC'},
     {label: 'Underline', style: 'UNDERLINE'},
-    {label: 'Monospace', style: 'CODE'}
 ];
